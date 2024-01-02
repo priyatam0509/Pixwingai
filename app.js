47b74ab0898c19a86ae67e4c996f6051f1e39ea7
priyatam0509 <piyushpriya34@gmail.com>
2024-01-03 00:20:28 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors')
+
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/send-email', (req, res) => {
   const {email, message } = req.body;
